745c2cff6075d41c5a7763666dd1fd10dbf3435b
Deepanjali Agarwal <deepanjaliagarwal@example.com>
2025-10-31 14:36:49 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Generative AI Project",
+    version="0.1",
+    author="Deepanjali",
+    author_email="deep1993agarwal@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
+    
